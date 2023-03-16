137912329707fe57aae26d965de7fdb21d74c1bf
Kham En <khamen28@gmail.com>
2023-03-16 10:51:35 -0400

    Add prompt of the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Editor from "./components/Editor";
+import Prompter from "./components/Prompter";
 
 function App() {
   return (
     <div className="p-2">
+      <div className="mx-auto my-8 max-w-[8.5in]">
+        <Prompter />
+      </div>
       <Editor />
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,12 +13,22 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        mono: ["Inconsolata", ...defaultTheme.fontFamily.serif],
+        serif: ["Handlee", ...defaultTheme.fontFamily.serif],
+        mono: ["Courier Prime", ...defaultTheme.fontFamily.mono],
+        sans: ["Source Sans", ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        "fluid-base": "clamp(1.13rem, calc(1.01rem + 0.57vw), 1.38rem)",
+        "fluid-base": "clamp(1.00rem, calc(0.89rem + 0.57vw), 1.25rem)",
+      },
+      spacing: {
+        "letter-height": "11in",
+        "letter-width": "8.5in",
+        "letter-fluid-vertical-padding":
+          "clamp(0.5rem, -0.6364rem + 5.6818vw, 3rem)",
+        "letter-fluid-horizontal-padding":
+          "clamp(1rem, -1.2727rem + 11.3636vw, 6rem)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
