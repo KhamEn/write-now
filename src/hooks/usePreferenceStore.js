e0a89fa62620ab77a60a3074a1caaf3bb481c103
Kham En <khamen28@gmail.com>
2023-07-25 17:17:55 -0400

    (enhance) Change default time and word count targets

--- src/hooks/usePreferenceStore.js
+++ src/hooks/usePreferenceStore.js
@@ -2,14 +2,14 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 const DEFAULT_HOURS = 0;
-const DEFAULT_MINUTES = 5;
-const DEFAULT_SECONDS = 30;
+const DEFAULT_MINUTES = 1;
+const DEFAULT_SECONDS = 0;
 const DEFAULT_MILLISECONDS =
   DEFAULT_HOURS * 3600000 + DEFAULT_MINUTES * 60000 + DEFAULT_SECONDS * 1000;
 
 const preferenceStore = (set, get) => ({
   wordCounterIsEnabled: true,
-  targetWordCount: 10,
+  targetWordCount: 20,
   timerIsEnabled: true,
   targetTimeInMilli: DEFAULT_MILLISECONDS,
 
